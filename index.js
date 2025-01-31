3c3d734e83179f1de1ffb79a066f94edd05ec922
t <git@example.com>
2025-01-30 22:59:54 -0500

    download all, handle errors

--- index.js
+++ index.js
@@ -1,44 +1,68 @@
 import fs from 'node:fs/promises'
 import express from 'express'
+import archiver from 'archiver'
+
+// TODO
+// handle sub-directories
 
 const PORT = 9999
+const ZIP_FILE_NAME = 'files.zip'
 
 let app = express()
 app.set('view engine', 'pug')
 app.use('/static', express.static('static'))
 
 // Home Page
-app.get('/', async (request, response)=> {
-  let files = await fs.readdir('./public/')
-  response.render('index', { files })
+app.get('/', async (request, response, next)=> {
+  try {
+    let files = await fs.readdir('./public/')
+    response.render('index', { files })
+  }
+  catch (error) { next(error) }
 })
 
 // Download All Files
-app.get('/download-all', (request, response)=> {
-  // todo:
-  // zip up da whole directory
-  // https://cheatcode.co/blog/how-to-create-and-download-a-zip-file-with-node-js-and-javascript
-  // response.download( ~~raw zip file~~ )
-  response.end()
+app.get('/download-all', (request, response, next)=> {
+  try {
+    response.writeHead(200, {
+      "Content-Type": "application/octet-stream",
+      "Content-Disposition": "attachment; filename=" + ZIP_FILE_NAME
+    })
+    let zip = archiver('zip', { zlib: { level: 9 } })
+    zip.on('warning', next)
+    zip.on('error', next)
+    zip.pipe(response)
+    zip.directory('./public/', false)
+    zip.finalize()
+  }
+  catch (error) { next(error) }
 })
 
 // Download File
-app.get('/download/:fileName', async (request, response)=> {
-  let fileName = request.params.fileName
-  let files = await fs.readdir('./public/')
-  if (files.includes(fileName)) response.download('public/' + fileName)
-  else response.end()
+app.get('/download/:fileName', async (request, response, next)=> {
+  try {
+    let fileName = request.params.fileName
+    response.download('public/' + fileName)
+  }
+  catch (error) { next(error) }
 })
 
 // Delete File
-app.get('/delete/:fileName', async (request, response)=> {
-  let fileName = request.params.fileName
-  let files = await fs.readdir('./public/')
-  if (files.includes(fileName)) {
+app.get('/delete/:fileName', async (request, response, next)=> {
+  try {
+    let fileName = request.params.fileName
+    let files = await fs.readdir('./public/')
     await fs.unlink('public/' + fileName)
     response.redirect('/')
   }
-  else response.end()
+  catch (error) { next(error) }
 })
 
+// Error Handler
+app.use((error, request, response, next)=> {
+  console.error(error.stack)
+  response.render('error', { error: error.stack })
+})
+
+// Start Server
 app.listen(PORT, ()=> { console.log("Listening on port", PORT)})
--- static/script.js
+++ static/script.js
@@ -1,1 +1,4 @@
-console.log("HELLO, WORLD!")
+let deleteFile = function(fileName) {
+  let ok = confirm(`Permanently delete ${fileName}?`)
+  if(ok) window.location.href = '/delete/' + fileName
+}
